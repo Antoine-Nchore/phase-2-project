3b4ca4ec39b3d2cd08c8e5784f140af9e860c7ca
James <james.ekasiba@student.moringaschool.com>
2023-11-06 22:28:56 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <Button variant="warning">Warning</Button>
+    </>
+  );
+}
+
+export default App;
