ef53720c8f64b021fd862333615c48548865cad2
James <james.ekasiba@student.moringaschool.com>
2023-11-07 11:00:24 +0300

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import Button from 'react-bootstrap/Button';
+import React, {useEffect, useState} from 'react';
+
+
+const baseURL = "http://localhost:8001/products";
 
 function App() {
+  const [products, setProducts] = useState([])
+  useEffect(()=> {
+    fetch(baseURL)
+    .then((res)=> res.json())
+    .then((data)=> setProducts(data))
+
+  },[]);
+
   return (
     <>
     <h1>Hello World</h1>
