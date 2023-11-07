f159d077aa2b402aa191103ecdca7af4b478784c
hillary <korirhillary67@gmail.com>
2023-11-07 17:44:24 +0300

    creating a card

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,1 +1,28 @@
-// write your code here.
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+function Results({products}) {
+  return (
+    <Row>
+        {products.map((products)=>(
+        <Col>
+    <Card style={{ width: '25rem',height: "42rem", marginTop: "20px" }}>
+      <Card.Img variant="top" src={products.image} style={{height: "350px"}}/>
+      <Card.Body>
+        <Card.Title><h2>{products.title}</h2></Card.Title>
+        <Card.Text>
+          ${products.price}
+        </Card.Text>
+        <Button variant="warning">Add to Cart</Button>
+      </Card.Body>
+    </Card>
+    </Col>
+    ))}
+    </Row>
+  );
+}
+
+export default Results;
