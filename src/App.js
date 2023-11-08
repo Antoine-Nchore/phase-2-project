12069cd104b68489b1d5686687b6bb11e778ed7b
Brenda <brendakungu3@gmail.com>
2023-11-08 12:20:57 +0300

    search bar done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Button from 'react-bootstrap/Button';
-
+import SearchBar from './Components/SearchBar'
 function App() {
   return (
     <>
     <h1>Hello World</h1>
     <Button variant="warning">Warning</Button>
+    <div className='search-bar-container'>
+      <SearchBar/>
+    </div>
     </>
   );
 }
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,1 +1,14 @@
-//write code here
+  import React from 'react'
+  import {FaSearch} from 'react-icons/fa'
+  import './SearchBar.css'
+
+   const SearchBar = () => {
+    return (
+      <div className='input-wrapper'>
+      <FaSearch id='search-icon'/>
+      <input placeholder='search here..'/>
+      </div>
+    )
+  }
+  export default SearchBar;
+  
