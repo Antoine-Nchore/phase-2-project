dd7ed160cdc785578fd5d190730e212e22b17477
Brenda <brendakungu3@gmail.com>
2023-11-13 11:15:37 +0300

    updated searchbar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
 
 
 function SearchBar({ onSearch }) {
@@ -11,18 +12,39 @@ function SearchBar({ onSearch }) {
     onSearch(text);
   };
   return (
-     <InputGroup className="mb-3" style={{width: "50rem", marginTop: "20px", marginLeft: "30px"}}>
-        <Form.Control
+    <InputGroup
+      className="mb-3"
+      style={{
+        width: "50rem",
+        marginTop: "20px",
+        marginLeft: "30px",
+        alignItems: "center",
+      }}
+    >
+      <Button variant="light">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+      </Button>
+      <Form.Control
         type="search"
-          placeholder="search product..."
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
-          value={searchText}
-      onChange={handleSearch}
-        />
-      </InputGroup>
+        placeholder="search product..."
+        aria-label="Recipient's username"
+        aria-describedby="basic-addon2"
+        value={searchText}
+        onChange={handleSearch}
+      />
+    </InputGroup>
   );
 }
 
 
 export default SearchBar;
+
